29400920cd4afa6ee7c0ef71b7de9039939463d5
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-03-08 21:47:15 +0530

    Modified Linked List

--- build/linked-list.js
+++ build/linked-list.js
@@ -1,20 +1,80 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var LinkedList = /** @class */ (function () {
-    function LinkedList(val) {
+var LinkedListNode = /** @class */ (function () {
+    function LinkedListNode(val) {
         this.val = val;
         this.next = null;
-        this.head = this;
     }
-    LinkedList.prototype.insert = function (val) {
-        var temp = this.head;
-        while (temp && temp.next) {
-            temp = temp.next;
-        }
+    return LinkedListNode;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    LinkedList.prototype.push_back = function (val) {
+        var temp = this.tail;
+        var newNode = new LinkedListNode(val);
         if (temp) {
-            temp.next = new LinkedList(val);
-            console.log("Inserted ".concat(val));
+            temp.next = newNode;
+            this.tail = temp.next;
         }
+        else {
+            this.head = this.tail = newNode;
+        }
+        this.size++;
+        return this.head;
+    };
+    LinkedList.prototype.push_front = function (val) {
+        var temp = this.head;
+        var newNode = new LinkedListNode(val);
+        if (temp) {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        else {
+            this.head = this.tail = newNode;
+        }
+        this.size++;
+        return this.head;
+    };
+    LinkedList.prototype.pop_back = function () {
+        if (this.size === 0)
+            return this.head;
+        if (this.size === 1) {
+            this.head = null;
+            this.tail = null;
+            this.size--;
+            return this.head;
+        }
+        var temp = this.head;
+        if (temp) {
+            while (temp && temp.next !== this.tail) {
+                temp = temp.next;
+            }
+            if (temp === null || temp === void 0 ? void 0 : temp.next)
+                temp.next = null;
+            this.tail = temp;
+            this.size--;
+            return this.head;
+        }
+    };
+    LinkedList.prototype.pop_front = function () {
+        var _a, _b;
+        if (this.size === 0)
+            return this.head;
+        if (this.size === 1) {
+            this.size--;
+            this.head = this.tail = null;
+            return this.head;
+        }
+        if ((_a = this.head) === null || _a === void 0 ? void 0 : _a.next) {
+            this.head = (_b = this.head) === null || _b === void 0 ? void 0 : _b.next;
+            this.size--;
+        }
+        return this.head;
     };
     LinkedList.prototype.delete = function (val) {
         var temp = this.head;
@@ -22,8 +82,11 @@ var LinkedList = /** @class */ (function () {
         if (temp && temp.val === val) {
             temp = temp.next;
             this.head = temp;
-            console.log("Deleted ".concat(val));
-            return;
+            this.size--;
+            if (this.size === 0) {
+                this.tail = this.head;
+            }
+            return this.head;
         }
         while (temp && temp.next && temp.next.val !== val) {
             temp = temp.next;
@@ -33,10 +96,10 @@ var LinkedList = /** @class */ (function () {
             var nodeToBeDeleted = temp.next;
             temp.next = nodeToBeDeleted.next;
             nodeToBeDeleted.next = null;
-            console.log("Deleted ".concat(val));
-            return;
+            this.size--;
+            return this.head;
         }
-        console.log("Not Found ".concat(val));
+        return this.head;
     };
     LinkedList.prototype.print = function () {
         var temp = this.head;
@@ -47,15 +110,13 @@ var LinkedList = /** @class */ (function () {
     };
     return LinkedList;
 }());
-var myList = new LinkedList(3);
-myList.insert(4);
-myList.insert(5);
-myList.insert(6);
+var myList = new LinkedList();
+myList.push_front(1);
+myList.push_front(2);
+myList.push_back(3);
+myList.push_back(4);
+myList.pop_back();
+myList.pop_front();
 myList.delete(3);
-myList.delete(5);
-myList.delete(6);
-myList.delete(8);
-myList.delete(4);
-myList.delete(9);
 myList.print();
 exports.default = LinkedList;
--- src/linked-list.ts
+++ src/linked-list.ts
@@ -1,34 +1,98 @@
-class LinkedList {
+class LinkedListNode {
   val: number;
-  next: LinkedList | null;
-  head: LinkedList | null;
+  next: LinkedListNode | null;
 
   constructor(val: number) {
     this.val = val;
     this.next = null;
-    this.head = this;
+  }
+}
+class LinkedList {
+  head: LinkedListNode | null;
+  tail: LinkedListNode | null;
+  size: number = 0;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
 
-  insert(val: number) {
-    let temp: LinkedList | null = this.head;
-    while (temp && temp.next) {
-      temp = temp.next;
-    }
+  push_back(val: number) {
+    let temp: LinkedListNode | null = this.tail;
+    const newNode = new LinkedListNode(val);
     if (temp) {
-      temp.next = new LinkedList(val);
-      console.log(`Inserted ${val}`);
+      temp.next = newNode;
+      this.tail = temp.next;
+    } else {
+      this.head = this.tail = newNode;
     }
+    this.size++;
+    return this.head;
+  }
+
+  push_front(val: number) {
+    let temp: LinkedListNode | null = this.head;
+    const newNode = new LinkedListNode(val);
+
+    if (temp) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      this.head = this.tail = newNode;
+    }
+    this.size++;
+    return this.head;
+  }
+
+  pop_back() {
+    if (this.size === 0) return this.head;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+      this.size--;
+      return this.head;
+    }
+
+    let temp: LinkedListNode | null = this.head;
+    if (temp) {
+      while (temp && temp.next !== this.tail) {
+        temp = temp.next;
+      }
+      if (temp?.next) temp.next = null;
+      this.tail = temp;
+      this.size--;
+      return this.head;
+    }
+  }
+
+  pop_front() {
+    if (this.size === 0) return this.head;
+    if (this.size === 1) {
+      this.size--;
+      this.head = this.tail = null;
+      return this.head;
+    }
+    if (this.head?.next) {
+      this.head = this.head?.next;
+      this.size--;
+    }
+    return this.head;
   }
 
   delete(val: number) {
-    let temp: LinkedList | null = this.head;
+    let temp: LinkedListNode | null = this.head;
 
     // Deleting First Node
     if (temp && temp.val === val) {
       temp = temp.next;
       this.head = temp;
-      console.log(`Deleted ${val}`);
-      return;
+      this.size--;
+
+      if (this.size === 0) {
+        this.tail = this.head;
+      }
+      return this.head;
     }
 
     while (temp && temp.next && temp.next.val !== val) {
@@ -41,15 +105,14 @@ class LinkedList {
       temp.next = nodeToBeDeleted.next;
       nodeToBeDeleted.next = null;
 
-      console.log(`Deleted ${val}`);
-      return;
+      this.size--;
+      return this.head;
     }
-
-    console.log(`Not Found ${val}`);
+    return this.head;
   }
 
   print() {
-    let temp: LinkedList | null = this.head;
+    let temp: LinkedListNode | null = this.head;
     while (temp) {
       console.log(temp.val);
       temp = temp.next;
@@ -57,16 +120,14 @@ class LinkedList {
   }
 }
 
-const myList = new LinkedList(3);
-myList.insert(4);
-myList.insert(5);
-myList.insert(6);
+const myList = new LinkedList();
+myList.push_front(1);
+myList.push_front(2);
+myList.push_back(3);
+myList.push_back(4);
+myList.pop_back();
+myList.pop_front();
 myList.delete(3);
-myList.delete(5);
-myList.delete(6);
-myList.delete(8);
-myList.delete(4);
-myList.delete(9);
 myList.print();
 
 export default LinkedList;
