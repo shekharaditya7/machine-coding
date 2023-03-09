bc98fee26316576980761841ce1a0c5421c42f3d
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-03-09 23:48:58 +0530

    Added data structures export

---
+++ src/data-structures/index.ts
@@ -0,0 +1,9 @@
+import DoublyLinkedList from "./doubly-linked-list";
+import LinkedList from "./linked-list";
+import LRUCache from "./lru-cache";
+
+module.exports = {
+  DoublyLinkedList,
+  LinkedList,
+  LRUCache,
+};
